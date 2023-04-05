5d41178dd8507af810b09b0dddeb6636f2f74f9b
Jacob Chisholm <jacobchisholm1010@gmail.com>
2023-04-05 14:38:24 -0400

    Create undefined.cbl

---
+++ undefined.cbl
@@ -0,0 +1,14 @@
+       program-id. undefined.
+
+       environment division.
+       configuration section.
+
+       data division.
+       working-storage section.
+       linkage section.
+
+       procedure division.
+           display "HI".
+           goback.
+
+       end program undefined.
